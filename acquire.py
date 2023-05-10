a5e8f22d10c0827f60bdd0269de0a4a5d1f8bfdd
Josh Burch <joshuaburch01@gmail.com>
2023-05-10 15:31:14 -0500

    added file

---
+++ acquire.py
@@ -0,0 +1,51 @@
+from requests import get
+from bs4 import BeautifulSoup as b
+
+import pandas as pd
+import numpy as np
+
+
+def get_blogs():
+    try:    
+        df = pd.read_csv('blogs.csv')
+    else FileNotFoundError:
+        url = 'https://codeup.com/blog/'
+        headers = {'User-Agent': 'Codeup Data Science'} # Some websites don't accept the pyhon-requests default user-agent
+        response = get(url, headers=headers)
+        links = []
+        for div in divs:
+            links.append(div.find('a')['href'])
+        blogs = []
+        for link in links:
+            blog = {}
+            response = get(link, headers=headers)
+            soup = b(response.content, 'html.parser')
+            title = soup.find('h2').text
+            p = soup.find_all('p')[4].text
+            blog.update({'title': title})
+            blog.update({'content': p})
+            blogs.append(blog)
+        df = pd.DataFrame(blogs)
+    return df
+
+
+
+def get_articles():
+    try:    
+        df = pd.read_csv('articles.csv')
+    else FileNotFoundError:
+        topics = ['business', 'sports', 'technology', 'entertainment']
+        articles = []
+        for topic in topics:
+            article={}
+            url = f'https://inshorts.com/en/read/{topic}'
+            response = get(url)
+            soup = b(response.content, 'html.parser')
+            title =soup.find_all('span', itemprop='headline')[0].text
+            content = soup.find_all('div', itemprop='articleBody')[0].text
+            content = content.replace('\n', '')
+            article.update({'title': title})
+            article.update({'content': content})
+            articles.append(article)
+        df = pd.DataFrame(articles)
+        return df
